c58683db71f48e65ab98952f312067a469cddaa0
Thomas McAndrew <tommcandrew@hotmail.com>
2020-06-14 20:05:13 +0100

    fix conflict

--- client/src/store/reducers/newQuiz.js
+++ client/src/store/reducers/newQuiz.js
@@ -1,32 +1,24 @@
-import { ADD_NEW_QUESTION, DELETE_QUESTION } from '../actions/newQuiz'
-
+import { ADD_NEW_QUESTION, DELETE_QUESTION } from "../actions/newQuiz";
 
 const initalState = {
-    quizQuestions: []
-}
+  quizQuestions: [],
+};
 
 export default (state = initalState, action) => {
-    switch (action.type) {
-        case ADD_NEW_QUESTION: 
-            return {
-               ...state, 
-                quizQuestions : state.quizQuestions.concat(action.question)
-            }
-        case DELETE_QUESTION: 
-            return {
-                ...state,
-                quizQuestions: state.quizQuestions.filter(
-                    question => question.id !== action.id
-                )
-            }
-<<<<<<< Updated upstream
-        
-=======
-        default: 
-            return state
->>>>>>> Stashed changes
-            
-    }
-     return state;
-}
-
+  switch (action.type) {
+    case ADD_NEW_QUESTION:
+      return {
+        ...state,
+        quizQuestions: state.quizQuestions.concat(action.question),
+      };
+    case DELETE_QUESTION:
+      return {
+        ...state,
+        quizQuestions: state.quizQuestions.filter(
+          (question) => question.id !== action.id
+        ),
+      };
+    default:
+      return state;
+  }
+};
