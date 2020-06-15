551095f8b90b191e5a3b0ecdcc7996c303db67d3
Thomas McAndrew <tommcandrew@hotmail.com>
2020-06-16 00:05:45 +0100

    fix remaining conflicts

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,15 @@
-import React, {useEffect} from 'react';
-import {Provider} from 'react-redux';
-import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
-import ReduxThunk from 'redux-thunk';
-import './app.css';
-import newQuizReducer from './store/reducers/newQuiz';
-import errorReducer from './store/reducers/errorReducer';
-import authReducer from './store/reducers/authReducer';
-import MainNavigation from './components/navigation/MainNavigation'
-import theme from './style/theme'
-import { ThemeProvider } from '@material-ui/core/styles';
-
+import React from "react";
+import { Provider } from "react-redux";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import ReduxThunk from "redux-thunk";
+import "./app.css";
+import errorReducer from "./store/reducers/errorReducer";
+import authReducer from "./store/reducers/authReducer";
+import userReducer from "./store/reducers/userReducer";
+import quizReducer from "./store/reducers/quizReducer";
+import MainNavigation from "./components/navigation/MainNavigation";
+import theme from "./style/theme";
+import { ThemeProvider } from "@material-ui/core/styles";
 
 const rootReducer = combineReducers({
   quiz: quizReducer,
@@ -18,20 +18,9 @@ const rootReducer = combineReducers({
   user: userReducer,
 });
 
-import Register from './screens/authenticationScreens/Register'
-
-
-export default function AppWrapper () {
-  const rootReducer = combineReducers ({
-    newQuiz: newQuizReducer,
-    error: errorReducer,
-    auth: authReducer,
-  });
-  const composeEnhancers = typeof window === 'object' &&
-  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-  ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ (
-      {
-
+const composeEnhancers =
+  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
         // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
       })
     : compose;
@@ -41,9 +30,9 @@ export const store = createStore(rootReducer, enhancer);
 export default function AppWrapper() {
   return (
     <Provider store={store}>
-      <ThemeProvider theme = {theme}>
-      <MainNavigation /></ThemeProvider>
-
+      <ThemeProvider theme={theme}>
+        <MainNavigation />
+      </ThemeProvider>
     </Provider>
   );
 }
