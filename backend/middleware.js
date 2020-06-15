fe8417f1c47a64a83489eabb6343095b1c63758a
Thomas McAndrew <tommcandrew@hotmail.com>
2020-06-15 18:03:08 +0100

    get user from jwt token instead of hardcode in backend

--- backend/middleware.js
+++ backend/middleware.js
@@ -1,12 +1,10 @@
 const jwt = require("jsonwebtoken");
 
 function auth(req, res, next) {
-  console.log("checking token");
   const token = req.header("x-auth-token");
 
   //chceck for token
   if (!token) {
-    console.log("no token");
     return res.status(401).json({ message: "No token, authorization denied" });
   } else {
     try {
@@ -14,10 +12,8 @@ function auth(req, res, next) {
       const decoded = jwt.verify(token, "secretkey");
       //add user from payload
       req.user = decoded;
-      console.log("everythings grand");
       next();
     } catch (e) {
-      console.log("tokens not valid");
       res.status(400).json({
         message: "Token is not valid",
       });
