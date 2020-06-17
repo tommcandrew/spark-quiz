08f41b77c01f7eb60497f2dfb040e7ad9d42df3e
ZehraRiz <52386212+ZehraRiz@users.noreply.github.com>
2020-06-18 00:36:56 +0300

    Update errorActions.js

--- client/src/store/actions/errorActions.js
+++ client/src/store/actions/errorActions.js
@@ -1,5 +1,5 @@
-export const GET_ERRORS = "GET_ERRORS";
-export const CLEAR_ERRORS = "CLEAR_ERRORS"
+export const GET_ERRORS = 'GET_ERRORS';
+export const CLEAR_ERRORS = 'CLEAR_ERRORS'
 
 export const returnErrors = (msg, status, id = null) => {
     return {
