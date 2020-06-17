17ffc502e7027d40807d69ca92364e90da1040af
ZehraRiz <52386212+ZehraRiz@users.noreply.github.com>
2020-06-18 00:39:14 +0300

    Update camelToSentence.js

--- client/src/utils/camelToSentence.js
+++ client/src/utils/camelToSentence.js
@@ -1,6 +1,6 @@
 const camelToSentence = string => {
-  const result = string.replace(/([A-Z])/g, " $1");
-  const finalResult = result.charAt(0).toUpperCase() + result.slice(1);
+  const result = string.replace (/([A-Z])/g, ' $1');
+  const finalResult = result.charAt (0).toUpperCase () + result.slice (1);
   return finalResult;
 };
 
