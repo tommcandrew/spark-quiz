2674d0f9a5d4ebecc3287d8f8777ce8f3f85e3e7
ZehraRiz <52386212+ZehraRiz@users.noreply.github.com>
2020-06-18 00:26:04 +0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -21,8 +21,8 @@ const rootReducer = combineReducers({
 const composeEnhancers =
   typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
-    })
+        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+      })
     : compose;
 const enhancer = composeEnhancers(applyMiddleware(ReduxThunk));
 export const store = createStore(rootReducer, enhancer);
