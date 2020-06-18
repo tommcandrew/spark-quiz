434c71bb9a0a164e5e510a359a01ccf06b882a37
Thomas McAndrew <tommcandrew@hotmail.com>
2020-06-18 18:13:32 +0100

    implement basic animation between questions

---
+++ client/src/components/UI/QuizOption.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const QuizOption = ({ option }) => {
+  return <div className="quizOption__wrapper">{option.text}</div>;
+};
+
+export default QuizOption;
--- client/src/screens/userScreens/Quiz.js
+++ client/src/screens/userScreens/Quiz.js
@@ -2,14 +2,47 @@ import React, { useEffect, useState } from "react";
 import gsap from "gsap";
 import questions from "./questions";
 import "./Quiz.css";
+import QuizOption from "../../components/UI/QuizOption";
 
 const Quiz = () => {
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+
+  const tl = gsap.timeline();
+
+  const handleClick = () => {
+    tl.to([".quiz__question", ".quiz__options"], 0.5, {
+      x: "-100vw",
+      opacity: 0,
+      ease: "Power4.easeIn",
+      onComplete: () => setCurrentQuestionIndex(currentQuestionIndex + 1),
+    });
+    tl.to([".quiz__question", ".quiz__options"], 0, {
+      x: 0,
+    });
+    tl.to(
+      [".quiz__question", ".quiz__options"],
+      1,
+      {
+        opacity: 1,
+        ease: "Power1.easeOut",
+      },
+      ">+0.3"
+    );
+  };
+
   return (
     <div className="quiz__wrapper">
       <div className="quiz__content">
-        <div className="quiz__question"></div>
-        <div className="quiz__options"></div>
+        <div className="quiz__question">
+          {questions[currentQuestionIndex].question}
+        </div>
+        <div className="quiz__options">
+          {questions[currentQuestionIndex].options.map((option) => (
+            <QuizOption option={option} />
+          ))}
+        </div>
       </div>
+      <button onClick={handleClick}>Next</button>
     </div>
   );
 };
