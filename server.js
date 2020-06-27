a186a9ad556398e82837b0e5908dcaefb137d51c
Thomas McAndrew <tommcandrew@hotmail.com>
2020-06-27 15:46:24 +0100

    create modal for updating contact info

--- server.js
+++ server.js
@@ -456,18 +456,36 @@ app.post("/addContact", auth, (req, res) => {
 app.post("/deleteContact", auth, (req, res) => {
   const id = req.user.id;
   const { contactId } = req.body;
-  console.log(typeof contactId);
-  console.log(contactId);
   User.findById(id)
     .then((user) => {
       const updatedContacts = user.contacts.filter(
         (contact) => contact._id.toString() !== contactId
       );
-      console.log(updatedContacts);
       user.contacts = updatedContacts;
       user.save().then(() => {
-        console.log("contact deleted");
-        res.status(200).send();
+        res.status(200).send({ msg: "Contact deleted" });
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.post("/updateContact", auth, (req, res) => {
+  const id = req.user.id;
+  const { contactId, updatedContact } = req.body;
+  User.findById(id)
+    .then((user) => {
+      user.contacts.forEach((contact) => {
+        if (contact._id.toString() === contactId) {
+          console.log("found contact with matching id");
+          contact.name = updatedContact.name;
+          contact.email = updatedContact.email;
+        }
+      });
+      user.save().then(() => {
+        console.log("contact updated");
+        res.status(200).send({ msg: "Contact deleted" });
       });
     })
     .catch((err) => {
