ee1c51a7a8970b1a3ebd3c55f0b604ab8d5abcc8
ZehraRiz <zehrataqi@gmail.com>
2020-07-14 06:49:41 +0300

    saving each question when answer clicked

--- routes/auth.js
+++ routes/auth.js
@@ -9,151 +9,157 @@ const User = require("../models/User.model");
 const validator = require("validator");
 
 router.post("/register", async (req, res) => {
-  const { name, email, password, password2 } = req.body;
-  if (password.length < 8) {
-    res.status(500).send({ msg: "Password must have at least 8 characters" });
-    return;
-  }
-  if (!validator.isEmail(email)) {
-    res.status(400).send({ msg: "Please enter a valid email address" });
-    return;
-  }
-  if (password !== password2) {
-    res.status(400).send({ msg: "Passwords must match" });
-    return;
-  }
-  const existingUser = await User.findOne({ email });
-  if (existingUser) {
-    res.status(403).send({ msg: "That email is already registered" });
-    return;
-  } else {
-    const salt = await bcrypt.genSalt(10);
-    const hash = await bcrypt.hash(password, salt);
-    const user = await new User({ name, email, password: hash }).save();
-    const token = jwt.sign(
-      { id: user._id, role: "teacher", name: user.name },
-      "secretkey"
-    );
-    res.send({
-      token,
-      user: {
-        id: user._id,
-        name: user.name,
-        email: user.email,
-        role: "teacher",
-        quizzes: [],
-        contacts: [],
-        groups: [],
-      },
-    });
-  }
+	const { name, email, password, password2 } = req.body;
+	if (password.length < 8) {
+		res.status(500).send({ msg: "Password must have at least 8 characters" });
+		return;
+	}
+	if (!validator.isEmail(email)) {
+		res.status(400).send({ msg: "Please enter a valid email address" });
+		return;
+	}
+	if (password !== password2) {
+		res.status(400).send({ msg: "Passwords must match" });
+		return;
+	}
+	const existingUser = await User.findOne({ email });
+	if (existingUser) {
+		res.status(403).send({ msg: "That email is already registered" });
+		return;
+	} else {
+		const salt = await bcrypt.genSalt(10);
+		const hash = await bcrypt.hash(password, salt);
+		const user = await new User({ name, email, password: hash }).save();
+		const token = jwt.sign({ id: user._id, role: "teacher", name: user.name }, "secretkey");
+		res.send({
+			token,
+			user: {
+				id: user._id,
+				name: user.name,
+				email: user.email,
+				role: "teacher",
+				quizzes: [],
+				contacts: [],
+				groups: []
+			}
+		});
+	}
 });
 
 router.post("/login", async (req, res) => {
-  const { email, password } = req.body;
-  const user = await User.findOne({ email });
-  if (!user) {
-    res.status(403).send({ msg: "That email is not registered" });
-  } else {
-    try {
-      const isSame = await bcrypt.compare(password, user.password);
-      if (!isSame) {
-        res.status(403).send({ msg: "Wrong password" });
-      } else {
-        const token = jwt.sign(
-          { id: user._id, role: "teacher", name: user.name },
-          "secretkey"
-        );
-        res.status(200).send({
-          token,
-          user: {
-            id: user._id,
-            name: user.name,
-            email: user.email,
-            role: "teacher",
-          },
-        });
-      }
-    } catch (err) {
-      console.log(err);
-    }
-  }
+	const { email, password } = req.body;
+	const user = await User.findOne({ email });
+	if (!user) {
+		res.status(403).send({ msg: "That email is not registered" });
+	} else {
+		try {
+			const isSame = await bcrypt.compare(password, user.password);
+			if (!isSame) {
+				res.status(403).send({ msg: "Wrong password" });
+			} else {
+				const token = jwt.sign({ id: user._id, role: "teacher", name: user.name }, "secretkey");
+				res.status(200).send({
+					token,
+					user: {
+						id: user._id,
+						name: user.name,
+						email: user.email,
+						role: "teacher"
+					}
+				});
+			}
+		} catch (err) {
+			console.log(err);
+		}
+	}
 });
 
 router.post("/studentLogin", async (req, res) => {
-  const { studentCode } = req.body;
-  try {
-    const quizzes = await Quiz.find();
-    let found;
-    quizzes.forEach((quiz) => {
-      quiz.quizInvites.contacts.forEach((contact) => {
-        if (contact.code === studentCode) {
-          found = contact.code;
-          const token = jwt.sign(
-            { code: contact.code, role: "student" },
-            "secretkey"
-          );
-          res.status(200).send({
-            quiz,
-            token,
-            user: {
-              code: contact.code,
-              contactId: contact.id,
-              role: "student",
-            },
-          });
-          return;
-        }
-      });
-    });
-    if (!found) {
-      res.status(403).send({ msg: "invalid code" });
-      return;
-    }
-  } catch (err) {
-    console.log(err);
-  }
+	const { studentCode } = req.body;
+	try {
+		const quizzes = await Quiz.find();
+		let found;
+		quizzes.forEach((quiz) => {
+			quiz.quizInvites.contacts.forEach( (contact) => {
+				if (contact.code === studentCode) {
+					quiz.quizScores.push({
+						studentId: contact.id,
+						results: [],
+						overallScore: 0
+					});
+					quiz.save()
+					found = contact.code;
+					const token = jwt.sign({ code: contact.code, role: "student" }, "secretkey");
+					res.status(200).send({
+						quiz: {
+							_id: quiz._id,
+							quizName: quiz.quizName,
+							quizAuthor: quiz.quizAuthor,
+							quizSubject: quiz.quizSubject,
+							quizQuestions: quiz.quizQuestions,
+							points: quiz.points,
+							quizTimeLimit: quiz.quizTimeLimit,
+							quizPointsSystem: quiz.quizPointsSystem,
+							quizOverallPoints: quiz.quizOverallPoints,
+							overallScore: quiz.overallScore
+						},
+						token,
+						user: {
+							code: contact.code,
+							contactId: contact.id
+						}
+					});
+				}
+			});
+		});
+		if (!found) {
+			res.status(403).send({ msg: "invalid code" });
+			return;
+		}
+	} catch (err) {
+		console.log(err);
+	}
 });
 
 router.post("/resetPassword", async (req, res) => {
-  const { email } = req.body;
-  try {
-    const user = await User.findOne({ email });
-    if (user) {
-      //need to generate random password
-      const tempPassword = "bananas";
-      const salt = await bcrypt.genSalt(10);
-      const hash = await bcrypt.hash(tempPassword, salt);
-      user.password = hash;
-      await user.save();
-      emailNewPassword(user.email);
-      res.status(200).send({ msg: "New password emailed" });
-    } else {
-      res.status(400).send({ msg: "User not found" });
-    }
-  } catch (err) {
-    console.log(err);
-  }
+	const { email } = req.body;
+	try {
+		const user = await User.findOne({ email });
+		if (user) {
+			//need to generate random password
+			const tempPassword = "bananas";
+			const salt = await bcrypt.genSalt(10);
+			const hash = await bcrypt.hash(tempPassword, salt);
+			user.password = hash;
+			await user.save();
+			emailNewPassword(user.email);
+			res.status(200).send({ msg: "New password emailed" });
+		} else {
+			res.status(400).send({ msg: "User not found" });
+		}
+	} catch (err) {
+		console.log(err);
+	}
 });
 
 router.post("/changepassword", checkAuth, async (req, res) => {
-  const { currentPassword, newPassword } = req.body;
-  const id = req.user.id;
-  try {
-    const user = await User.findById(id);
-    const isSame = await bcrypt.compare(currentPassword, user.password);
-    if (!isSame) {
-      res.status(403).send({ msg: "Passwords don't match" });
-    } else {
-      const salt = await bcrypt.genSalt(10);
-      const hash = await bcrypt.hash(newPassword, salt);
-      user.password = hash;
-      await user.save();
-      res.status(200).send({ msg: "Password changed" });
-    }
-  } catch (err) {
-    console.log(err);
-  }
+	const { currentPassword, newPassword } = req.body;
+	const id = req.user.id;
+	try {
+		const user = await User.findById(id);
+		const isSame = await bcrypt.compare(currentPassword, user.password);
+		if (!isSame) {
+			res.status(403).send({ msg: "Passwords don't match" });
+		} else {
+			const salt = await bcrypt.genSalt(10);
+			const hash = await bcrypt.hash(newPassword, salt);
+			user.password = hash;
+			await user.save();
+			res.status(200).send({ msg: "Password changed" });
+		}
+	} catch (err) {
+		console.log(err);
+	}
 });
 
 module.exports = router;
--- routes/student.js
+++ routes/student.js
@@ -1,50 +1,112 @@
 const express = require("express");
 const router = express.Router();
+const jwt = require("jsonwebtoken");
 const checkAuth = require("../middleware");
 const Quiz = require("../models/Quiz.model");
+const { update } = require("../models/Quiz.model");
 
-router.post("/saveScores", checkAuth, async (req, res) => {
-  let { _id, scoreObject } = req.body;
-  scoreObject = JSON.parse(scoreObject);
-  try {
-    const quiz = await Quiz.findById(_id);
-    quiz.quizScores.push(scoreObject);
-    await quiz.save();
-    res.status(200).send({ quiz });
-  } catch (err) {
-    res.status(400).send({ msg: "unable to add scores" });
-  }
+router.get("/quizReload", checkAuth, async (req, res) => {
+	const studentCode = req.user.code;
+	try {
+		const quizzes = await Quiz.find();
+		let found;
+		quizzes.forEach((quiz) => {
+			quiz.quizInvites.contacts.forEach((contact) => {
+				if (contact.code === studentCode) {
+					found = contact.code;
+					const token = jwt.sign({ code: contact.code, role: "student" }, "secretkey");
+					res.status(200).send({
+						quiz: {
+							_id: quiz._id,
+							quizName: quiz.quizName,
+							quizAuthor: quiz.quizAuthor,
+							quizSubject: quiz.quizSubject,
+							quizQuestions: quiz.quizQuestions,
+							points: quiz.points,
+							quizTimeLimit: quiz.quizTimeLimit,
+							quizPointsSystem: quiz.quizPointsSystem,
+							quizOverallPoints: quiz.quizOverallPoints,
+							overallScore: quiz.overallScore
+						},
+						token,
+						user: {
+							code: contact.code,
+							contactId: contact.id
+						}
+					});
+					return;
+				}
+			});
+		});
+		if (!found) {
+			res.status(403).send({ msg: "invalid code" });
+			return;
+		}
+	} catch (err) {
+		console.log(err);
+	}
+});
+
+router.post("/saveAnswer", checkAuth, async (req, res) => {
+	let { quizId, studentId, question, answer } = req.body;
+	try {
+		const quiz = await Quiz.findById(quizId);
+		const student = quiz.quizScores.find((std) => std.studentId === studentId);
+		student.results.push({ question: question, correct: answer });
+		await quiz.save();
+		res.status(200);
+	} catch (err) {
+		res.status(400).send({ msg: "unable to add scores" });
+	}
+});
+
+router.post("/saveScore", checkAuth, async (req, res) => {
+	let { quizId, studentId, newScore } = req.body;
+
+	try {
+		const quiz = await Quiz.findById(quizId);
+		const updatedRecord = quiz.quizScores.find((score) => score.studentId === studentId);
+		updatedRecord.overallScore = newScore;
+		await Quiz.findOneAndUpdate(
+			{ _id: quizId, "quizScores.studentId": studentId },
+			{ $set: { quizScores: updatedRecord } },
+			{ upsert: true }
+		);
+		res.status(200).send({ msg: "updated the Score" });
+	} catch (err) {
+		res.status(400).send({ err });
+	}
 });
 
 router.post("/submit", async (req, res) => {
-  const { studentId, _id, submittedAnswers } = req.body;
-  try {
-    const quiz = await Quiz.findById(_id);
-    let results = [];
-    quiz.questions.forEach((question, index) => {
-      if (question.questionType === "trueFalse") {
-        if (question.answers.trueFalseAnswer === submittedAnswers[index]) {
-          results.push({ question: index, correct: true });
-        } else {
-          results.push({ question: index, correct: false });
-        }
-      } else if (question.questionType === "multipleChoice") {
-        if (question.answers.multipleChoiceAnswer === submittedAnswers[index]) {
-          results.push({ question: index, correct: true });
-        } else {
-          results.push({ question: index, correct: false });
-        }
-      }
-    });
-    const scoreObject = {
-      studentId,
-      results,
-    };
-    quiz.quizScores.push(scoreObject);
-    res.status(200).send({ msg: "Quiz submitted", results });
-  } catch (err) {
-    console.log(err);
-  }
+	const { studentId, _id, submittedAnswers } = req.body;
+	try {
+		const quiz = await Quiz.findById(_id);
+		let results = [];
+		quiz.questions.forEach((question, index) => {
+			if (question.questionType === "trueFalse") {
+				if (question.answers.trueFalseAnswer === submittedAnswers[index]) {
+					results.push({ question: index, correct: true });
+				} else {
+					results.push({ question: index, correct: false });
+				}
+			} else if (question.questionType === "multipleChoice") {
+				if (question.answers.multipleChoiceAnswer === submittedAnswers[index]) {
+					results.push({ question: index, correct: true });
+				} else {
+					results.push({ question: index, correct: false });
+				}
+			}
+		});
+		const scoreObject = {
+			studentId,
+			results
+		};
+		quiz.quizScores.push(scoreObject);
+		res.status(200).send({ msg: "Quiz submitted", results });
+	} catch (err) {
+		console.log(err);
+	}
 });
 
 module.exports = router;
