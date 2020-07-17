dba68f1f87301d7a7243fd536b1a9fcd25011ea0
Thomas McAndrew <tommcandrew@hotmail.com>
2020-07-17 09:04:53 +0100

    only show stats button for published quiz

--- routes/auth.js
+++ routes/auth.js
@@ -82,6 +82,7 @@ router.post("/login", async (req, res) => {
 
 router.post("/studentLogin", async (req, res) => {
   const { studentCode } = req.body;
+  console.log(studentCode);
   try {
     const quizzes = await Quiz.find();
     let found;
@@ -107,6 +108,7 @@ router.post("/studentLogin", async (req, res) => {
       });
     });
     if (!found) {
+      console.log("quiz not found");
       res.status(403).send({ msg: "invalid code" });
       return;
     }
