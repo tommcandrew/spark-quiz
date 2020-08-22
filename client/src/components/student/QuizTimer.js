eb2a407219dd551321cf4fefce4c4812616d421d
Thomas McAndrew <tommcandrew@hotmail.com>
2020-08-22 10:03:34 +0100

    stop time up warning when no limit

--- client/src/components/student/QuizTimer.js
+++ client/src/components/student/QuizTimer.js
@@ -3,12 +3,18 @@ import React, { useState, useEffect } from "react";
 const Timer = ({ seconds, setTimeUp }) => {
   const [timeLeft, setTimeLeft] = useState(seconds);
 
+  console.log("TIMER");
+  useEffect(() => {
+    console.log(timeLeft);
+  }, [timeLeft]);
+
   function fmtMSS(s) {
     return (s - (s %= 60)) / 60 + (9 < s ? ":" : ":0") + s;
   }
 
   useEffect(() => {
     if (!timeLeft) {
+      console.log("TIME IS FALSY SO SETTING TIME UP TO TRUE");
       setTimeUp(true);
       return;
     }
