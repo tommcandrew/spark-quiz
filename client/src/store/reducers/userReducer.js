4541bc3f2c05f328c09f7ed939a564f01edecd0d
ZehraRiz <zehrataqi@gmail.com>
2020-08-24 20:21:48 +0300

    redux bug removed

--- client/src/store/reducers/userReducer.js
+++ client/src/store/reducers/userReducer.js
@@ -1,45 +1,45 @@
-import {
-  FETCH_QUIZZES,
-  DELETE_QUIZ,
-  ADD_CONTACT,
-  ADD_GROUP,
-  ADD_QUIZ,
-} from "../actions/userActions";
+import { FETCH_QUIZZES, DELETE_QUIZ, ADD_CONTACT, ADD_GROUP, ADD_QUIZ } from "../actions/userActions";
 
-import { QUIZZES_LOADED } from "../actions/authActions"
-
+import { QUIZZES_LOADED, CLEAR_USER_REDUCER } from "../actions/authActions";
 
 const initalState = {
-  quizzes: [],
-  contacts: [],
-  groups: [],
+	quizzes: [],
+	contacts: [],
+	groups: []
 };
 
 //changing cases to strings because of strange error
 export default (state = initalState, action) => {
-  switch (action.type) {
-    case FETCH_QUIZZES:
-      return { ...state, quizzes: [...action.payload] };
-    case QUIZZES_LOADED:
-      return {
-        ...state,
-        quizzes: action.payload
-      }
-    case DELETE_QUIZ:
-      return {
-        ...state,
-        quizzes: state.quizzes.filter((quiz) => quiz._id !== action.payload),
-      };
-    case ADD_QUIZ:
-      return {
-        ...state,
-        quizzes: [...state.quizzes, action.payload],
-      };
-    case ADD_CONTACT:
-      return { ...state, contacts: [...state.contacts, action.payload] };
-    case ADD_GROUP:
-      return { ...state, groups: [...state.groups, action.payload] };
-    default:
-      return state;
-  }
+	switch (action.type) {
+		case FETCH_QUIZZES:
+			return { ...state, quizzes: [ ...action.payload ] };
+		case QUIZZES_LOADED:
+			return {
+				...state,
+				quizzes: action.payload
+			};
+		case DELETE_QUIZ:
+			return {
+				...state,
+				quizzes: state.quizzes.filter((quiz) => quiz._id !== action.payload)
+			};
+		case ADD_QUIZ:
+			return {
+				...state,
+				quizzes: [ ...state.quizzes, action.payload ]
+			};
+		case ADD_CONTACT:
+			return { ...state, contacts: [ ...state.contacts, action.payload ] };
+		case ADD_GROUP:
+			return { ...state, groups: [ ...state.groups, action.payload ] };
+		case CLEAR_USER_REDUCER:
+			return {
+				...state,
+				quizzes: [],
+				contacts: [],
+				groups: []
+			};
+		default:
+			return state;
+	}
 };
